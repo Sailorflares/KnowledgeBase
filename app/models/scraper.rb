6255eb1f02dec3345cb04198a7e8a189f3e32960
Karim Butt <karim.butt@gmail.com>
2014-12-01 18:15:50 -0500

    added scraper

---
+++ app/models/scraper.rb
@@ -0,0 +1,19 @@
+class Scraper
+	
+	def initialize(topic)
+		url = "http://en.wikipedia.org/wiki/#{topic}"
+		@doc = Nokogiri::HTML(open(url))
+	end
+
+
+	def parse
+		results = Hash.new
+		@doc.css('div.toc ul li').each do |li|
+			number = li.css(".tocnumber").first.text.to_f
+			text = li.css(".toctext").first.text	
+			results[number]=text
+		end
+		results
+	end
+end
+
