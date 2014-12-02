dc8ae3ed325d6b308458a48e75d35a2289036b29
Elizabeth Wolensky <liz.wolensky@gmail.com>
2014-12-02 13:20:51 -0500

    nprapi now takes muliple search terms. transferrd to json output from npr

--- lib/npr_api.rb
+++ lib/npr_api.rb
@@ -1,26 +1,32 @@
 require 'open-uri'
-require 'nori'
 
 class NprApi
 
-  attr_reader :url, :xml_doc
+  attr_reader :url, :json
 
-  def initialize(search_term)
-    @url = create_query_url(search_term)
-    @xml_doc = get_xml
+  def initialize(search_terms)
+    @url = create_query_url(search_terms)
+    @json = get_json
   end
 
-  def create_query_url(search_term)
-    "http://api.npr.org/query?searchTerm=#{search_term}&apiKey=MDE3NTg1OTAzMDE0MTc0NjU3MDZhZTk4Yw001"
+  def create_query_url(search_terms)
+    url = "http://api.npr.org/query?output=JSON" 
+    search_terms.each do |term|
+      url + "&searchTerm=#{term}"
+    end
+    return url + "&apiKey=MDE3NTg1OTAzMDE0MTc0NjU3MDZhZTk4Yw001"
   end
 
-  def get_xml
-    Nokogiri::XML(open(url)).to_xml
+  def get_json
+    JSON.load(open(url))
   end
 
   def links_array
-    parser = Nori.new
-    parser.parse(get_xml)
+    array = []
+    get_json["list"]["story"].each do |story_hash|
+      array << story_hash["link"].first["$text"]
+    end
+    return array
   end
   
 end
