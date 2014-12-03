4a557a8797adbbaf9f6815e2057e83b7608d06c0
Elizabeth Wolensky <liz.wolensky@gmail.com>
2014-12-03 12:34:17 -0500

    individual topic pages work

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,6 @@
+class TopicsController < ApplicationController
+
+  def show
+    @topic = params[:topic]
+  end
+end
---
+++ lib/nyt_api.rb
@@ -0,0 +1,4 @@
+class NytApi
+
+  "http://api.nytimes.com/svc/search/v2/articlesearch.json?[q=#{searchterm}]&api-key=1a7b0ee104b10dc6baaa5e59392a0b17:8:70267967"
+end
