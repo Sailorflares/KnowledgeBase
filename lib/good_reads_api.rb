21627ecb98ea95af78b61c8cad1f2df603b89fae
Elizabeth Wolensky <liz.wolensky@gmail.com>
2014-12-03 22:37:08 -0500

    commit

--- lib/good_reads_api.rb
+++ lib/good_reads_api.rb
@@ -1,7 +1,24 @@
+require 'open-uri'
+require 'nori'
+
 class GoodReadsApi
   
-  def create_query_url
-    "https://www.goodreads.com/search/index.xml?key=#{ENV['GOOD_READS_KEY']}&q=#{{search_term}}"
-  end
+  attr_reader :hash
   
+  def initialize(search_term)
+    @url = create_query_url(search_term)
+    @hash = get_hash
+  end
+
+  def create_query_url(search_term)
+    "https://www.goodreads.com/search/index.xml?key=#{ENV['GOOD_READS_KEY']}&q=#{search_term}"
+  end
+
+  def get_hash
+    Nori.new.parse(Nokogiri::XML(open(@url)).to_xml)
+  end
+
+  def stories_array
+    get_hash["GoodreadsResponse"]["search"]["results"]["work"]
+  end
 end
