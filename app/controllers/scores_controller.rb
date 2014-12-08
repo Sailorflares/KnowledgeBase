764f1ec5f1f32a692fbb57443136bd3f5307aa3d
Elizabeth Wolensky <liz.wolensky@gmail.com>
2014-12-08 15:54:50 -0500

    addig classification via ajax

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,2 +1,7 @@
+require 'pry'
 class ScoresController < ApplicationController
+
+  def train_relevant
+    Score.train('topic', 'relevant', 'Definitely relative')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/topic', to: 'topics#show'
   get '/user', to: 'users#show'  
 
+  post '/relevant', to: 'scores#train_relevant'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
