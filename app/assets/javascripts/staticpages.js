8daaeaa817e3b65efdb20d7c740596619c763bde
Karim Butt <karim.butt@gmail.com>
2014-12-08 14:54:46 -0500

    completed about page

--- app/assets/javascripts/staticpages.js
+++ app/assets/javascripts/staticpages.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
-	$("h3").hide();
-	$("img.two").hide();
+	$(".one").hide();
+	$(".two").hide();
+	$(".three").hide();
 
-	$(".one").fadeIn();
-	$(".two").delay(2000).fadeIn();
-	$(".three").delay(4000).fadeIn();
-	$(".four").delay(6000).fadeIn();
+
+	$(".one").fadeIn(1000).delay(1000).fadeOut(1000).delay(6000).fadeIn();
+	$(".two").delay(3000).fadeIn(1000).delay(1000).fadeOut(1000).delay(3000).fadeIn();
+	$(".three").delay(6000).fadeIn(1000).delay(1000).fadeOut(1000).delay(0).fadeIn();
+
+
 })
