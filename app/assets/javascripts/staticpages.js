28500feec587832d75ea0b8e3a8c9abc68869820
Karim Butt <karim.butt@gmail.com>
2014-12-09 14:34:31 -0500

    slowed down about page animations

--- app/assets/javascripts/staticpages.js
+++ app/assets/javascripts/staticpages.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
 	$(".three").hide();
 
 
-	$(".one").fadeIn(1000).delay(1000).fadeOut(1000).delay(6000).fadeIn();
-	$(".two").delay(3000).fadeIn(1000).delay(1000).fadeOut(1000).delay(3000).fadeIn();
-	$(".three").delay(6000).fadeIn(1000).delay(1000).fadeOut(1000).delay(0).fadeIn();
+	$(".one").fadeIn(1000).delay(2000).fadeOut(1000).delay(8000).fadeIn();
+	$(".two").delay(4000).fadeIn(1000).delay(2000).fadeOut(1000).delay(4000).fadeIn();
+	$(".three").delay(8000).fadeIn(1000).delay(2000).fadeOut(1000).delay(0).fadeIn();
 
 
 })
